35f40f21bd23bb9c6a97fd8701d4a9b9dfba86d7
Areyouokay0 <natsudragneelfiredragongulid@gmail.com>
2022-11-29 00:07:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,15 +7,27 @@ API_ID = int(os.environ.get("API_ID", ""))
 
 API_HASH = os.environ.get("API_HASH", "")
 
-if __name__ == "__main__" :
-    plugins = dict(
-        root="plugins"
-    )
-    app = Client(
-        "renamer",
-        bot_token=TOKEN,
-        api_id=APP_ID,
-        api_hash=API_HASH,
-        plugins=plugins
-    )
-    app.run()
+class Bot(Client):
+
+    def __init__(self):
+        super().__init__(
+            name="renamer",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=TOKEN,
+            plugins={"root": "plugins"},
+        )
+
+    async def start(self):
+        await super().start()
+        #web-response
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, 8080).start()
+
+    async def stop(self, *args):
+        await super().stop()
+
+app = Bot()
+app.run()
